82f83ab72fbcadeae08b922b66448e65ac0781d2
JavierCatalanP <ingaciocatalanp@gmail.com>
2024-04-20 10:08:56 -0400

    Imprimir - Variables

---
+++ hola-mundo.js
@@ -0,0 +1,11 @@
+console.log("hola mundo");
+console.log("saludos");
+console.log('"Hola JS"');
+
+//Otras formas de imprimir son:
+
+//alert("Hola mundo");
+alert("Hola mundo js");
+//document.write("Hola mundo");
+
+document.write("<h1>Hola mundo</h1>");
---
+++ variables.js
@@ -0,0 +1,44 @@
+let miNumero1 = 15;
+console.log(miNumero1);
+
+//Reglas para nombres de variables
+//No puede iniciar con numeros
+//No puede contener espacios
+//No puede contener caracteres especiales
+//No puede contener palabras reservadas
+
+//ya no se usa var, se reemplaza por let,
+
+
+//camelCase - recomendada en js como buenas practicas 
+let miNombreCompleto = "John"; 
+console.log(miNombreCompleto);
+
+//PascalCase
+let MiNombreCompleto = "John";
+console.log(MiNombreCompleto);
+
+//snake_case
+let mi_nombre_completo = "John";
+console.log(mi_nombre_completo);
+
+//UPPERCASE
+let MI_NOMBRE_COMPLETO = "John";
+console.log(MI_NOMBRE_COMPLETO);
+
+//Concatenar
+let nombre = "John";
+let apellido = "Doe";
+
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+
+//Concatenar con template string
+let nombreCompleto2 = `${nombre} ${apellido}`;
+console.log(nombreCompleto2);
+
+
+//los comentarios son para que el programador entienda el codigo
+/*tambien se pueden
+utilizar 
+para texto asi*/
