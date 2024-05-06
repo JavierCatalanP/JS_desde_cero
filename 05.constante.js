161c77e4d1b601eb601b0bcbbd775fa722341c9e
JavierCatalanP <ingaciocatalanp@gmail.com>
2024-05-06 16:13:55 -0400

    05. Constantes

---
+++ 05.constante.js
@@ -0,0 +1,19 @@
+// Constante
+
+const MI_CONSTANTE = 10;
+
+//MI_CONSTANTE = 20; // No podemos camviar el valor de una constante.
+
+console.log(MI_CONSTANTE);
+console.log(Math.PI);
+
+// Crear una constante con numeros decimales
+
+const NUMERO_DECIMAL = 2.5;
+console.log(NUMERO_DECIMAL);
+
+//Constante segundo por minuto
+
+const SEGUNDOS_POR_MINUTIS = 60;
+console.log(SEGUNDOS_POR_MINUTIS);
+
