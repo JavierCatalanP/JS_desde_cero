b6b12b2f478698a442d989812ef44c4487c8b60f
JavierCatalanP <ingaciocatalanp@gmail.com>
2024-05-06 17:31:58 -0400

    06.Operadores Aritméticos

---
+++ 06.operadores.js
@@ -0,0 +1,51 @@
+//Operadores Aritméticos
+
+let a, b, c, d, e, f, g, h, i, j;
+
+// + Suma X + Y
+a = 3 + 4;
+console.log(a); 
+
+// - Resta X - Y
+b = 3 - 4;
+console.log(b); 
+
+// * Multiplicación x * y
+c = 3 * 4;
+console.log(c);
+
+// / División X / y
+d= 4 / 2.5;
+console.log(d);
+console.log(typeof d);
+
+// % Módulo (residuo división) X % Y
+f= 9%2;
+console.log(f); 
+// si divido 2 raiz cuadrada de 4 partido 9 me da - 8, la resta es 1.
+
+
+// ** Potencia (Exponente) X ** | ECMAScript 2'16
+g = 2 ** 3;  // 2 * 2 * = 8
+console.log(g); 
+
+//en versiones anteriores de JS
+h = Math.pow(2,3);
+console.log(h);
+
+
+
+
+// ++ Incremento ++X ó X++ 
+
+// -- Decremento --X ó X -- 
+
+//Operadores de asignación
+
+// = Asignación
+// += Asignación de suma
+// -= Asignación de resta
+// *= Asignación de multiplicación
+// /= Asignación de división    
+
+// /
